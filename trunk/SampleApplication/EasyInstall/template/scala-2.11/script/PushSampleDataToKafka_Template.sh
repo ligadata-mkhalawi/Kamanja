96407d4d2d34fbfad0b4b5c3b0cba17f4432fc7e
ligadata-dan <dan@ligadata.com>
2016-06-27 16:26:44 -0700

    add a --hostnames argument to the Push Scripts

--- trunk/SampleApplication/EasyInstall/template/scala-2.11/script/PushSampleDataToKafka_Template.sh
+++ trunk/SampleApplication/EasyInstall/template/scala-2.11/script/PushSampleDataToKafka_Template.sh
@@ -16,18 +16,30 @@ OPTION=useroption-1
 INPUTFILE=${LISTOFFILES[OPTION]}
 fi
 
-if [ "$KAMANJA_KERBEROS_CLIENT" ]; then
+kafkahostname="localhost:9092"
+while [ $# -gt 0 ]
+do
+    if [ "$1" == "--kafkahosts" ]; then
+    	kafkahostname="$2"
+    fi
+    shift
+done
+
+if [ "$KAMANJA_SEC_CONFIG" ]; then
   KAFKA_KERBEROS_CLIENT_OPT="-Djava.security.auth.login.config="$KAMANJA_SEC_CONFIG
+  echo "Using java.security.auth.login.config="$KAMANJA_SEC_CONFIG
 fi
 
-if [ "$KERBEROS_CONFIG" ]; then
+if [ "$KAMANJA_KERBEROS_CONFIG" ]; then
   KERBEROS_CONFIG_OPT="-Djava.security.krb5.confg="$KAMANJA_KERBEROS_CONFIG
+  echo "Using java.security.krb5.confg="$KAMANJA_KERBEROS_CONFIG
 fi
 
 if [ "$KAMANJA_SECURITY_CLIENT" ]; then
   SECURITY_PROP_OPT="--secprops "$KAMANJA_SECURITY_CLIENT
+  echo "Using security client = "KAMANJA_SECURITY_CLIENT
 fi
 
 echo "User selected: $INPUTFILE"
-java  $JAAS_CONFIG_OPT $KERBEROS_CONFIG_OPT -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-1.5.0.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-1.5.0.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-1.5.0.jar:$KAMANJA_HOME/lib/system/simplekafkaproducer_2.11-1.5.0.jar com.ligadata.tools.SimpleKafkaProducer --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files $INPUTFILE   --partitionkeyidxs "1" --format CSV $SECURITY_PROP_OPT
+java  $JAAS_CONFIG_OPT $KERBEROS_CONFIG_OPT -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-1.5.0.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-1.5.0.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-1.5.0.jar:$KAMANJA_HOME/lib/system/simplekafkaproducer_2.11-1.5.0.jar com.ligadata.tools.SimpleKafkaProducer --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "$kafkahostname" --files $INPUTFILE   --partitionkeyidxs "1" --format CSV $SECURITY_PROP_OPT
 
