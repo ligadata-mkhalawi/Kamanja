c302f1db448dced4cce3679b89daaf365f2777f7
ligadata-baraa <baraa@ligadata.com>
2016-06-29 23:54:56 +0300

    icons size tuning

--- trunk/KamanjaUI/UI/app/scripts/services/serviceConfig.js
+++ trunk/KamanjaUI/UI/app/scripts/services/serviceConfig.js
@@ -9,56 +9,56 @@ angular.module('networkApp')
           extension:'svg',
           iconColor: '#96f',
           headerColor: '#93f',
-          width: '25',
-          height: '25'
+          width: '45',
+          height: '45'
         },
         message: {
           image: 'message',
           extension:'svg',
           iconColor: '#fc9',
           headerColor: '#f93',
-          width: '25',
-          height: '25'
+          width: '45',
+          height: '45'
         },
         input: {
           image: 'input',
           extension:'svg',
           iconColor: '#cc6',
           headerColor: '#c93',
-          width: '25',
-          height: '25'
+          width: '45',
+          height: '45'
         },
         output: {
           image: 'output',
           extension:'svg',
           iconColor: '#3ff',
           headerColor: '#6cc',
-          width: '25',
-          height: '25'
+          width: '45',
+          height: '45'
         },
         storage: {
           image: 'storage',
           extension:'svg',
           iconColor: '#cf9',
           headerColor: '#9c3',
-          width: '25',
-          height: '25'
+          width: '45',
+          height: '45'
         },
         container: {
           image: 'container',
           extension:'svg',
           iconColor: '#909',
           headerColor: '#f6f',
-          width: '25',
-          height: '25'
+          width: '45',
+          height: '45'
         },
         system: {
           image: 'system',
           extension:'svg',
           iconColor: '#cc3',
           headerColor: '#663',
-          width: '25',
-          height: '25'
+          width: '45',
+          height: '45'
         },
         dag: {
           image: 'dag',
