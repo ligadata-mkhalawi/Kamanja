6867cfe79200008642df9d18c4d465de882fbec0
ligadata-pokuri <pokuri@ligadata.com>
2016-07-27 20:54:58 -0700

    Scripts which has few version changes

--- trunk/SampleApplication/EasyInstall/template/scala-2.11/script/FileDataConsumer_Template.sh
+++ trunk/SampleApplication/EasyInstall/template/scala-2.11/script/FileDataConsumer_Template.sh
@@ -12,4 +12,6 @@ if [ "$KAMANJA_KERBEROS_CONFIG" ]; then
   echo "Using java.security.krb5.confg="$KERBEROS_CONFIG_OPT
 fi
 
-java $JAAS_CLIENT_OPT $KERBEROS_CONFIG_OPT -Dlog4j.configurationFile=file:$KAMANJA_HOME/config/log4j2.xml -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-1.5.1.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-1.5.1.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-1.5.1.jar:$KAMANJA_HOME/lib/system/filedataconsumer_2.11-1.5.1.jar com.ligadata.filedataprocessor.LocationWatcher "$@"
+currentKamanjaVersion=1.5.1
+
+java $JAAS_CLIENT_OPT $KERBEROS_CONFIG_OPT -Dlog4j.configurationFile=file:$KAMANJA_HOME/config/log4j2.xml -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-${currentKamanjaVersion}.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-${currentKamanjaVersion}.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-${currentKamanjaVersion}.jar:$KAMANJA_HOME/lib/system/filedataconsumer_2.11-${currentKamanjaVersion}.jar com.ligadata.filedataprocessor.LocationWatcher "$@"
