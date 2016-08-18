506abd556d93b55c74947ddefddf20475c24900c
Muhammad Majali <majali01@gmail.com>
2016-08-18 13:34:01 +0300

    adding new flare project.

---
+++ trunk/KamanjaUI/flare/app/scripts/flareApp.js
@@ -0,0 +1,23 @@
+/**
+ * Created by muhammad on 8/18/16.
+ */
+'use strict'
+angular.module('flareApp', ['ngAnimate', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise(function ($injector, $location) {
+      var $state = $injector.get('$state');
+      if ($location.$$path === '' || $location.$$path === '/') {
+        $state.go('/');
+      } else
+        $state.go('404');
+    });
+
+    $stateProvider
+      .state('/', {
+        templateUrl: 'views/main.html',
+        controller: 'mainCtrl as main'
+      })
+      .state('404', {
+        templateUrl: 'views/404.html'
+      });
+  });
