d36d9787dc4717b43432133b53d44b5c2663ce54
flare <flare@flare.ligadata>
2016-08-25 11:47:11 -0400

    fixed template

--- trunk/SampleApplication/EasyInstall/template/scala-2.11/script/Socket_Template.sh
+++ trunk/SampleApplication/EasyInstall/template/scala-2.11/script/Socket_Template.sh
@@ -3,5 +3,4 @@
 KAMANJA_HOME={InstallDirectory}
 
 currentKamanjaVersion=1.5.3
-
-java -Dlog4j.configurationFile=file:$KAMANJA_HOME/config/log4j2.xml -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-${currentKamanjaVersion}.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-${currentKamanjaVersion}.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-${currentKamanjaVersion}.jar:$KAMANJA_HOME/lib/system/socket_2.11-${currentKamanjaVersion}.jar com.ligadata.MetaDataApiClient.KamanjaService "$@"
+java -cp /opt/KAM/Kamanja-${currentKamanjaVersion}_2.11/lib/system/KamanjaInternalDeps_2.11-${currentKamanjaVersion}.jar:/opt/KAM/Kamanja-1.5.3_2.11/lib/system/ExtDependencyLibs2_2.11-${currentKamanjaVersion}.jar:/opt/KAM/Kamanja-1.5.3_2.11/lib/system/ExtDependencyLibs_2.11-${currentKamanjaVersion}.jar:/opt/KAM/Kamanja-${currentKamanjaVersion}_2.11/lib/system/kamanjamanager_2.11-${currentKamanjaVersion}.jar:$KAMANJA_HOME/lib/system/socket_2.11-${currentKamanjaVersion}.jar com.ligadata.MetaDataApiClient.KamanjaService
