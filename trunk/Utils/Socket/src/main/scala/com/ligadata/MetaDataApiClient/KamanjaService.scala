6fcc921d37b5abd2900817eb85383683a62570fe
flare <flare@flare.ligadata>
2016-08-28 12:18:10 -0400

    added properties file for the three components configs

--- trunk/Utils/Socket/src/main/scala/com/ligadata/MetaDataApiClient/KamanjaService.scala
+++ trunk/Utils/Socket/src/main/scala/com/ligadata/MetaDataApiClient/KamanjaService.scala
@@ -8,12 +8,14 @@ import org.apache.logging.log4j.LogManager
 import scala.actors.threadpool.{TimeUnit, Executors}
 //import sys.process._
 import java.io.File
+import java.io.FileInputStream
 import scala.io.Source
 import com.ligadata.kamanja.metadata.MdMgr._
 import com.ligadata.MetadataAPI.MetadataAPIImpl
 /**
   * Created by Yasser on 5/24/2016.
   */
+import java.util.Properties
 object KamanjaService {
 
   private val LOG = LogManager.getLogger(getClass)
@@ -21,30 +23,16 @@ object KamanjaService {
 
 
   def main(args : Array[String]) : Unit = {
-	val getMetadataAPI = MetadataAPIImpl.getMetadataAPI
-        val filePath = "/opt/KAM/Kamanja-1.5.3_2.11/config/MetadataAPIConfig.properties"
-        val config = Source.fromFile(filePath).mkString
-	getMetadataAPI.InitMdMgrFromBootStrap(filePath, false)
-	val mdMgr = GetMdMgr
-	val tenantInfo = mdMgr.GetAllTenantInfos
-	
-    	if (tenantInfo.length<2) {
-      	  LOG.warn("Uploading cluster config")
-	  val ccFilePath = new File("/opt/KAM/Kamanja-1.5.3_2.11/config/ClusterConfig_kafka_v10.json")
- 	  val cfgDef = Source.fromFile(ccFilePath).mkString
-          val response=MetadataAPIImpl.UploadConfig(cfgDef, None, "configuration")
+	if(args.length<1){
+          LOG.error("KAMANJA-Service: Sending shutdown request due to user not providing components Properties file")
+          KamanjaConfiguration.shutdown = true // Setting the global shutdown
+          shutdown()
+	  sys.exit(1)
 	}
-    	else {
-    	  LOG.warn("cluster config already uploaded")
-    	}
-
-    scala.sys.addShutdownHook({
-      if (!KamanjaConfiguration.shutdown) {
-        LOG.warn("KAMANJA-Service: Received shutdown request")
-        KamanjaConfiguration.shutdown = true // Setting the global shutdown
-        shutdown()
-      }
-    })
+	 val (metadataFile:String,clusterConfigFile:String,engineConfigFile:String,port:String)=loadPropertiesFile(args(0))
+	println(metadataFile+"\n"+clusterConfigFile+"\n"+engineConfigFile+"\n"+port)
+          verifyClusterCfgLoaded(metadataFile,clusterConfigFile)
+	
 
 
     //run engine
@@ -57,7 +45,7 @@ object KamanjaService {
         LOG.warn("starting KAMANJA-MANAGER")
 
         //TODO : pass engine config file path (hardcoded temporarily)
-        val engineCfg = Array[String]("--config", "/opt/KAM/Kamanja-1.5.3_2.11/config/Engine1Config.properties")
+        val engineCfg = Array[String]("--config", engineConfigFile)
         LOG.warn("KamanjaService - main() : engineCfg.length="+engineCfg.length)
         KamanjaManager.startKamanjaManager(engineCfg)
       }
@@ -70,8 +58,7 @@ object KamanjaService {
         LOG.warn("starting Metadata API")
 
         //TODO : pass config file path, and port (hardcoded temporarily)
-        val msgApiCfg = Array[String]("--config", "/opt/KAM/Kamanja-1.5.3_2.11/config/MetadataAPIConfig.properties",
-          "--port", "9000")
+        val msgApiCfg = Array[String]("--config", metadataFile,"--port",port)
         MetadataAPIManager.start(msgApiCfg)
       }
     }
@@ -83,7 +70,7 @@ object KamanjaService {
         LOG.warn("starting Rest APIService")
 
         //TODO : pass config file path (hardcoded temporarily)
-        val msgApiCfg = Array[String]("--config", "/opt/KAM/Kamanja-1.5.3_2.11/config/MetadataAPIConfig.properties")
+        val msgApiCfg = Array[String]("--config", metadataFile)
 
         APIService.startAPISevrice(msgApiCfg)
       }
@@ -114,4 +101,50 @@ object KamanjaService {
 
     }
   }
+def loadPropertiesFile(propFile:String)={
+          try{
+	    val prop=new Properties()
+            prop.load(new FileInputStream(propFile))
+            (
+                (prop.getProperty("MetadataFile"),
+                prop.getProperty("ClusterConfigFile"),
+                prop.getProperty("EngineConfigFile"),
+                prop.getProperty("Port")
+            )
+)
+          }
+          catch{ case e: Exception =>
+            e.printStackTrace()
+            KamanjaConfiguration.shutdown = true // Setting the global shutdown
+            shutdown()
+
+         }
+}
+ 
+def verifyClusterCfgLoaded( metadataCfgFile:String,clusterConfigFile:String)={
+     val getMetadataAPI = MetadataAPIImpl.getMetadataAPI
+        val filePath = "/opt/KAM/Kamanja-1.5.3_2.11/config/MetadataAPIConfig.properties"
+        val config = Source.fromFile(filePath).mkString
+        getMetadataAPI.InitMdMgrFromBootStrap(filePath, false)
+        val mdMgr = GetMdMgr
+        val tenantInfo = mdMgr.GetAllTenantInfos
+
+        if (tenantInfo.length<2) {
+          LOG.warn("Uploading cluster config")
+          val ccFilePath = new File(clusterConfigFile)
+          val cfgDef = Source.fromFile(ccFilePath).mkString
+          val response=MetadataAPIImpl.UploadConfig(cfgDef, None, "configuration")
+        }
+        else {
+          LOG.warn("cluster config already uploaded")
+        }
+       scala.sys.addShutdownHook({
+          if (!KamanjaConfiguration.shutdown) {
+          LOG.warn("KAMANJA-Service: Received shutdown request")
+          KamanjaConfiguration.shutdown = true // Setting the global shutdown
+          shutdown()
+        }
+    })
+	
+}
 }
