4d7e514822132ab00c7c28c1e56dfcffaf5b34b7
ligadata-ramana <ramana@ligadata.com>
2016-08-31 00:26:02 -0700

    Issue 1159, allow a custom config file when startig engine using command kamanja

--- trunk/SampleApplication/EasyInstall/template/scala-2.11/script/StartEngine_Cassandra_Template.sh
+++ trunk/SampleApplication/EasyInstall/template/scala-2.11/script/StartEngine_Cassandra_Template.sh
@@ -15,8 +15,21 @@ fi
 
 currentKamanjaVersion=1.6.0
 
+ENGINECONFIG="$KAMANJA_HOME/config/Engine1Config_cassandra.properties"
+DEBUG="NO"
+if [ "$1" != "" ]; then
+    if [ "$1" = "debug" ]; then
+	DEBUG="YES"
+    else
+	ENGINECONFIG=$1
+	if [ "$2" = "debug" ]; then
+	    DEBUG="YES"
+	fi
+    fi
+fi
+
 if [ "$1" != "debug" ]; then
-	java  $JAAS_CONFIG_OPT $KERBEROS_CONFIG_OPT -Dlog4j.configurationFile=file:{InstallDirectory}/config/log4j2.xml -cp {InstallDirectory}/lib/system/ExtDependencyLibs2_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/ExtDependencyLibs_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/KamanjaInternalDeps_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/kamanjamanager_2.11-${currentKamanjaVersion}.jar com.ligadata.KamanjaManager.KamanjaManager --config {InstallDirectory}/config/Engine1Config_Cassandra.properties
+	java  $JAAS_CONFIG_OPT $KERBEROS_CONFIG_OPT -Dlog4j.configurationFile=file:{InstallDirectory}/config/log4j2.xml -cp {InstallDirectory}/lib/system/ExtDependencyLibs2_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/ExtDependencyLibs_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/KamanjaInternalDeps_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/kamanjamanager_2.11-${currentKamanjaVersion}.jar com.ligadata.KamanjaManager.KamanjaManager --config $ENGINECONFIG
 else
-	java  $JAAS_CONFIG_OPT $KERBEROS_CONFIG_OPT -Xdebug -Xrunjdwp:transport=dt_socket,address="$ipport",server=y -Dlog4j.configurationFile=file:{InstallDirectory}/config/log4j2.xml -cp {InstallDirectory}/lib/system/ExtDependencyLibs2_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/ExtDependencyLibs_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/KamanjaInternalDeps_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/kamanjamanager_2.11-${currentKamanjaVersion}.jar com.ligadata.KamanjaManager.KamanjaManager --config {InstallDirectory}/config/Engine1Config_Cassandra.properties
+	java  $JAAS_CONFIG_OPT $KERBEROS_CONFIG_OPT -Xdebug -Xrunjdwp:transport=dt_socket,address="$ipport",server=y -Dlog4j.configurationFile=file:{InstallDirectory}/config/log4j2.xml -cp {InstallDirectory}/lib/system/ExtDependencyLibs2_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/ExtDependencyLibs_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/KamanjaInternalDeps_2.11-${currentKamanjaVersion}.jar:{InstallDirectory}/lib/system/kamanjamanager_2.11-${currentKamanjaVersion}.jar com.ligadata.KamanjaManager.KamanjaManager --config $ENGINECONFIG
 fi
