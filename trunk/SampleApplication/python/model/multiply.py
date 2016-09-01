9ced90daacbdad196302d6cab81aece026a6dc3f
Hari Ramasamy <hariramasamy@ligadata.com>
2016-09-01 11:55:19 -0700

    KAMANJA-435 - KAMANJA 1034

--- trunk/SampleApplication/python/model/multiply.py
+++ trunk/SampleApplication/python/model/multiply.py
@@ -15,36 +15,3 @@ class MultiplyTuple(ModelInstance):
 		outMsg = json.dumps({'a' : msg["a"], 'b' : msg["b"], 'result' : prodofTups})
 		return outMsg
 
-	def __init__(self, host, port, modelOptions, logger):
-		""" One might want to configure the model here with modelOptions info"""
-		""" See getInputOutputFields(self) below for an example of how to access"""
-		""" modelOptions"""
-		super(MultiplyTuple, self).__init__(host, port, modelOptions, logger)
-
-	def getInputOutputFields(self):
-		"""The field names and their types needed by the model are returned to """
-		"""the python proxy (model stub communicating with this server). """
-		"""Feel free to just hard code the type info if that is best. """
-		"""The returned dictionaries are used by the python proxy to choose """
-		"""which fields from the associated messages(s) to send to the python server """
-		"""when the model is executed.  This is appropriate when the message contains"""
-		"""a thousand fields, but the model only uses five of them. """
-
-		"""As shown, conceivably the information could be configured in the model """
-		"""options. """
-
-		self.logger.debug("Entered MultiplyTuple.getInputOutputFields")
-		modelOptions = super(MultiplyTuple, self).ModelOptions()
-		inputFields = dict()
-		outputFields = dict()
-		if "InputTypeInfo" in modelOptions and "OutputTypeInfo" in modelOptions:
-			inputFields.update(modelOptions["InputTypeInfo"])
-			outputFields.update(modelOptions["OutputTypeInfo"])
-		else:
-			inputFields["a"] = "Int"
-			inputFields["b"] = "Int"
-			outputFields["a"] = "Int"
-			outputFields["b"] = "Int"
-			outputFields["result"] = "Int"
-
-		return (inputFields , outputFields)
--- trunk/SampleApplication/python/model/theanoExample.py
+++ trunk/SampleApplication/python/model/theanoExample.py
@@ -21,37 +21,3 @@ class AddTheanoTuple(ModelInstance):
                 outMsg = json.dumps({'a' : msg["a"], 'b' : msg["b"], 'result' : sumofTup.item(0)})
                 return outMsg
 
-        def __init__(self, host, port, modelOptions, logger):
-                """ One might want to configure the model here with modelOptions info"""
-                """ See getInputOutputFields(self) below for an example of how to access"""
-                """ modelOptions"""
-                super(AddTheanoTuple, self).__init__(host, port, modelOptions, logger)
-
-        def getInputOutputFields(self):
-                """The field names and their types needed by the model are returned to """
-                """the python proxy (model stub communicating with this server). """
-                """Feel free to just hard code the type info if that is best. """
-                """The returned dictionaries are used by the python proxy to choose """
-                """which fields from the associated messages(s) to send to the python server """
-                """when the model is executed.  This is appropriate when the message contains"""
-                """a thousand fields, but the model only uses five of them. """
-
-                """As shown, conceivably the information could be configured in the model """
-                """options. """
-
-                self.logger.debug("Entered AddTheanoTuple.getInputOutputFields")
-                modelOptions = super(AddTheanoTuple, self).ModelOptions()
-                inputFields = dict()
-                outputFields = dict()
-                if "InputTypeInfo" in modelOptions and "OutputTypeInfo" in modelOptions:
-                        inputFields.update(modelOptions["InputTypeInfo"])
-                        outputFields.update(modelOptions["OutputTypeInfo"])
-                else:
-                        inputFields["a"] = "Float"
-                        inputFields["b"] = "Float"
-                        outputFields["a"] = "Float"
-                        outputFields["b"] = "Float"
-                        outputFields["result"] = "Float"
-
-                return (inputFields , outputFields)
-
