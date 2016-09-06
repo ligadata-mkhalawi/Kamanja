2c36029c699a16a123c7be63d3962399e60cc4b2
Hari Ramasamy <hariramasamy@ligadata.com>
2016-09-06 10:11:17 -0700

    KAMANJA-435, KAMANJA-1037

--- trunk/SampleApplication/LoanRisk/model/LoanRisk.scala
+++ trunk/SampleApplication/LoanRisk/model/LoanRisk.scala
@@ -27,12 +27,29 @@ class LoanRiskFactory(modelDef: ModelDef, nodeContext: NodeContext) extends Mode
 
 class LoanRisk(factory: ModelInstanceFactory) extends ModelInstance(factory) {
    override def execute(txnCtxt: TransactionContext, execMsgsSet: Array[ContainerOrConcept], triggerdSetIndex: Int, outputDefault: Boolean): Array[ContainerOrConcept] = {
-     var helloWorld : msg1 =  execMsgsSet(0).asInstanceOf[msg1] // This run should trigger when we have only msg1
-        if(helloWorld.score!=1)
-          return null;
-		val output = outmsg1.createInstance().asInstanceOf[outmsg1];
-		output.id = helloWorld.id;
-		output.name = helloWorld.name;
-        return Array(output);
+       var loanRisk : msg1 =  execMsgsSet(0).asInstanceOf[msg1] // This run should trigger when we have only msg1
+       var pred : Float = -1.49786052275209
+       pred += loanRisk.TIN_IMO_REASON_DebtCon *  -0.239668692936379
+       pred += loanRisk.TIN_IZR_JOB_Mgr *  1.37932908138322 
+       pred += loanRisk.TIN_IZR_JOB_Office * 0.775391491255473
+       pred += loanRisk.TIN_IZR_JOB_Other * 1.44907154775426
+       pred += loanRisk.TIN_IZR_JOB_ProfExe * 1.40707813621254
+       pred += loanRisk.TIN_IZR_JOB_Sales * 2.38272021895632
+       pred += loanRisk.TIN_IZR_JOB_Self * 1.97738454381451
+       pred += loanRisk.R01_LOAN * -1.98016770987421
+       pred += loanRisk.IMN_R01_MORTDUE * -1.70893083156384
+       pred += loanRisk.IMN_R01_VALUE * 46.8834972899893
+       pred += loanRisk.IMN_R01_YOJ * -0.610183852644824
+       pred += loanRisk.IMN_R01_DEROG * 4.47856184348009
+       pred += loanRisk.IMN_R01_DELINQ * 7.37620839328494
+       pred += loanRisk.IMN_R01_CLAGE * -6.85708989347486
+       pred += loanRisk.IMN_R01_NINQ * 2.89418521536115
+       pred += loanRisk.IMN_R01_CLNO * -0.296037079316927
+       pred += loanRisk.LOG6_DEROG * 0.390988196099627
+       pred += loanRisk.LOG5_DELINQ * 1.8255237489947
+       pred += loanRisk.LOG_VALUE * -35.3593065292401
+       output.PYTHON_RISK_SCORE = pred
+       output.rec_id  = loanRisk.rec_id 
+       return Array(output);
    }
 }
