9f29b98ec451b5e14b50e582af879184f530c9de
ligadata-haitham <haitham@ligadata.com>
2016-09-19 15:23:55 -0700

    use transactionId and rowId instead of 0,0

--- trunk/InputOutputAdapters/ElasticsearchAdapters/src/main/scala/com/ligadata/ElasticsearchInputOutputAdapters/ElasticsearchProducer.scala
+++ trunk/InputOutputAdapters/ElasticsearchAdapters/src/main/scala/com/ligadata/ElasticsearchInputOutputAdapters/ElasticsearchProducer.scala
@@ -198,7 +198,7 @@ class ElasticsearchProducer(val inputConfig: AdapterConfiguration, val nodeConte
 
     val data_list = outputContainers.groupBy(_.getFullTypeName.toLowerCase).map(oneContainerData => {
       (oneContainerData._1, oneContainerData._2.map(container => {
-        (Key(container.TimePartitionData(), container.PartitionKeyData(), 0, 0), "", container.asInstanceOf[Any])
+        (Key(container.TimePartitionData(), container.PartitionKeyData(), container.getTransactionId, container.getRowNumber), "", container.asInstanceOf[Any])
       }))
     }).toArray
 
